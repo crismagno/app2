8b04329d7e180d6adfcf5843f2bfee8551149f83
@cmendonca1 <cmendonca@drcontactlens.com>
2021-12-04 23:38:25 -0300

    Improve Script

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-// import Script from "next/script";
+import Script from "next/script";
 import Head from "next/head";
 import "bootstrap/dist/css/bootstrap.css";
 import "../styles/globals.css";
@@ -10,6 +10,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>App 2</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+
         {/* bootstrap */}
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
@@ -17,11 +18,6 @@ function MyApp({ Component, pageProps }) {
           integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
           crossOrigin="anonymous"
         />
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-          crossOrigin="anonymous"
-        ></script>
 
         {/* fontawesome */}
         <link
@@ -37,6 +33,11 @@ function MyApp({ Component, pageProps }) {
           href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"
         ></link>
       </Head>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+        crossOrigin="anonymous"
+      ></Script>
       <Component {...pageProps} />
     </>
   );
