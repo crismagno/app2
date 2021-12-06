c0cb7795226ce9fee9830db3f8d83601b42358e3
@cmendonca1 <cmendonca@drcontactlens.com>
2021-12-06 00:28:16 -0300

    textarea memo

--- src/containers/Chat/Textarea/index.tsx
+++ src/containers/Chat/Textarea/index.tsx
@@ -1,4 +1,4 @@
-import { IconPlaneAlt, IconSmileAlt } from "../../../components/General/icons";
+import { memo } from "react";
 
 interface ChatTextareaProps {
   onKeypress: (event: any) => void;
@@ -15,38 +15,39 @@ export const ChatTextarea: React.FC<ChatTextareaProps> = ({
   setIsVisiblePicker,
   value,
 }): JSX.Element => {
-  const colorBtnPicker = `bg-gradient-to-tr from-gray-500 to-gray-600
-    hover:bg-gradient-to-tr hover:from-gray-600 hover:to-gray-700`;
-
-  const colorBtnSend = `bg-gradient-to-tr from-blue-500 to-blue-600
-    hover:bg-gradient-to-tr hover:from-blue-600 hover:to-blue-700`;
-
   return (
     <div className={"d-flex flex-row align-items-center"}>
       <textarea
         onKeyPress={onKeypress}
         className="shadow-none form-control"
-        onChange={(event) => onChange(event)}
+        onChange={onChange}
         value={value}
         style={{ resize: "none" }}
-        placeholder={`Mensagem...`}
+        placeholder={`Message...`}
       />
       <button
-        className={`btn ${colorBtnPicker} text-white mt-1 shadow-none h-100 mb-1 ml-1 px-4 mx-1`}
+        className={`
+          btn bg-gradient-to-tr from-gray-500 to-gray-600
+          hover:bg-gradient-to-tr hover:from-gray-600 hover:to-gray-700
+          text-white mt-1 
+          shadow-none h-100 mb-1 ml-1 px-4 mx-1
+        `}
         onClick={setIsVisiblePicker}
       >
         <i className="far fa-smile" />
-        {/* {IconSmileAlt(5)} */}
       </button>
       <button
-        className={`btn ${colorBtnSend} text-white mt-1 shadow-none h-100 mb-1 ml-0 px-4`}
+        className={`
+          btn bg-gradient-to-tr from-blue-500 to-blue-600
+          hover:bg-gradient-to-tr hover:from-blue-600 hover:to-blue-700
+        text-white mt-1 shadow-none h-100 mb-1 ml-0 px-4
+        `}
         onClick={sendMessageInvoke}
       >
         <i className="far fa-paper-plane" />
-        {/* {IconPlaneAlt(5)} */}
       </button>
     </div>
   );
 };
 
-export default ChatTextarea;
+export default memo(ChatTextarea);
